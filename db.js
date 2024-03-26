d988e65802466cf7c184d5eed9880893c7882cba
SatyamMishra49 <skm28112001@gmail.com>
2024-03-26 11:18:04 +0530

    init

---
+++ db.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb+srv://admin123:Satyam1749@projectdb.ahlnsia.mongodb.net/?retryWrites=true&w=majority&appName=projectdb")
+
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
+});
+
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+});
+
+const Account = mongoose.model('Account', accountSchema);
+const User = mongoose.model('User', userSchema);
+
+module.exports = {
+	User,
+  Account,
+};
